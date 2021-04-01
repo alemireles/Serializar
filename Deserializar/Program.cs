a8088c179391649a105c43edad06af6796573d93
luismireles92 <44795089+luismireles92@users.noreply.github.com>
2021-03-31 18:56:53 -0600

    Cargar SOlucion

---
+++ Deserializar/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Deserializar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            XmlSerializer mySerializer = new XmlSerializer(typeof(Object));
+            byte[] byteArray = Encoding.UTF8.GetBytes("String");
+            MemoryStream stream = new MemoryStream(byteArray);
+            Object comprobante = (Object)mySerializer.Deserialize(stream);
+
+
+            XmlSerializer mySerializer2 = new XmlSerializer(typeof(Object));
+            byte[] byteArray2 = Encoding.UTF8.GetBytes("String");
+            FileStream filestram = File.Open("", FileMode.Open);
+            Object comprobante2 = (Object)mySerializer.Deserialize(filestram);
+
+        }
+    }
+}
